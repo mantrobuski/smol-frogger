7ed26f61893cc626a01cdc0960fec820f238ea17
Mason <spellbattle4@gmail.com>
2022-05-06 16:35:12 -0600

    frog model and road/water

--- game.js
+++ game.js
@@ -2,17 +2,22 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const framerate = 1000.0/(50.0); //change the denom to whatever framerate you want
 
-//document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keydown", keyDownHandler, false);
 //document.addEventListener("keyup", keyUpHandler, false);
 
-const maxTime = 100;
-let time = maxTime;
+const maxTime = 100; //int
+let time = maxTime; //int
+
+let x = 4; y = 8; //int
+
+//8x9 grid, grid squares are 8x12 pixels
 
 function draw()
 {
 	//clears canvas each frame before drawing
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+	/*
 	//this is temp, but there are 3 lanes of cars, one safe lane in between, then 3 lanes of water, last and first lane are goal and starting point respectively.
 	for(var i = 0; i < 9; i++)
 	{
@@ -22,7 +27,67 @@ function draw()
 
 		ctx.fillRect(0, i * 12, 64, 12);
 	}
+	*/
 
+	time--;
+	if (time == 0) time = maxTime;
+
+	background();
+	frog();
+	timeScore();
+	
+}
+setInterval(draw, framerate);
+
+function background()
+{
+	//top middle end
+	ctx.fillStyle = 'green';
+	ctx.fillRect(0, 96, 64, 12);
+	ctx.fillRect(0, 0, 64, 12);
+	ctx.fillRect(0, 48, 64, 12);
+
+	//road
+	ctx.fillStyle = 'black';
+	ctx.fillRect(0, 60, 64, 36);
+
+	//water
+	ctx.fillStyle = 'blue';
+	ctx.fillRect(0, 12, 64, 36);
+}
+
+function frog()
+{
+	ctx.fillStyle = 'red';
+	//ctx.fillRect(x*8 + 1, y*12 + 1, 6, 10);
+
+	//head
+	ctx.fillRect(x*8 + 2, y*12 + 2, 1, 1);
+	ctx.fillRect(x*8 + 5, y*12 + 2, 1, 1);
+	ctx.fillStyle = 'lightgreen';
+	ctx.fillRect(x*8 + 2, y*12 + 3, 4, 1);
+
+	//body
+	ctx.fillRect(x*8 + 3, y*12 + 1, 2, 7);
+
+	//arms
+	ctx.fillRect(x*8, y*12 + 3, 1, 1);
+	ctx.fillRect(x*8 + 1, y*12 + 4, 1, 2);
+	ctx.fillRect(x*8 + 1, y*12 + 5, 6, 1);
+	ctx.fillRect(x*8 + 6, y*12 + 4, 1, 1);
+	ctx.fillRect(x*8 + 7, y*12 + 3, 1, 1);
+
+	//legs
+	ctx.fillRect(x*8 + 2, y*12 + 8, 4, 1);
+	ctx.fillRect(x*8 + 1, y*12 + 9, 1, 1);
+	ctx.fillRect(x*8, y*12 + 8, 1, 3);
+	ctx.fillRect(x*8 + 6, y*12 + 9, 1, 1);
+	ctx.fillRect(x*8 + 7, y*12 + 8, 1, 3);
+	
+}
+
+function timeScore()
+{
 	ctx.fillStyle = 'black';
 
 	//side bars
@@ -31,61 +96,31 @@ function draw()
 
 	//percent of bar to fill
 	let ratio = time / maxTime;
-	ctx.fillRect(4, 111, Math.round(ratio * 56), 4)
+	ctx.fillRect(4, 111, Math.round(ratio * 56), 4);
 
 	ctx.font = '10px Silkscreen';
 	ctx.fillText('100 HI: 150', 0, 128);
-
-	time--;
-	if (time == 0) time = maxTime;
-
-	
 }
-setInterval(draw, framerate);
 
-/*
 function keyDownHandler(e)
 {
-	if(e.key == 'Up' || e.key == 'ArrowUp' || e.key == 'w') input.up = true;
-	else if(e.key == 'Down' || e.key == 'ArrowDown' || e.key == 's') input.down = true;
-	else if(e.key == 'Left' || e.key == 'ArrowLeft' || e.key == 'a') input.left = true;
-	else if(e.key == 'Right' || e.key == 'ArrowRight' || e.key == 'd') input.right = true;
-	
-	//ignore upcoming if the input hasn't changed since last time
-	if(inputEquals(input, lastInput)) return;
+	if(e.key == 'Up' || e.key == 'ArrowUp' || e.key == 'w') y-=1;
+	else if(e.key == 'Down' || e.key == 'ArrowDown' || e.key == 's') y+=1;
+	else if(e.key == 'Left' || e.key == 'ArrowLeft' || e.key == 'a') x-=2;
+	else if(e.key == 'Right' || e.key == 'ArrowRight' || e.key == 'd') x+=2;
 
-	if(e.key == 'Up' || e.key == 'ArrowUp' || e.key == 'w') control = 0;
-	else if(e.key == 'Down' || e.key == 'ArrowDown' || e.key == 's') control = 1;
-	else if(e.key == 'Left' || e.key == 'ArrowLeft' || e.key == 'a') control = 2;
-	else if(e.key == 'Right' || e.key == 'ArrowRight' || e.key == 'd') control = 3;
-	
-	//shallow copy of input object
-	lastInput = {...input};
-
-	//console.log(control, e.key);
+	if(x > 7) x = 7;
+	else if(x < 0) x = 0;
+	if(y > 8) y = 8;
+	else if(y < 0) y = 0;
 }
 
+/*
 function keyUpHandler(e)
-{
-	//this is so that if you're holding two keys and let go of one it will keep the direction of the remaining key
-	
+{	
 	if(e.key == 'Up' || e.key == 'ArrowUp' || e.key == 'w') input.up = false;
 	else if(e.key == 'Down' || e.key == 'ArrowDown' || e.key == 's') input.down = false;
 	else if(e.key == 'Left' || e.key == 'ArrowLeft' || e.key == 'a') input.left = false;
 	else if(e.key == 'Right' || e.key == 'ArrowRight' || e.key == 'd') input.right = false;
-
-	//ignore upcoming if the input hasn't changed since last time
-	if(inputEquals(input, lastInput)) return;
-
-	if(input.up) control = 0;
-	else if(input.down) control = 1;
-	else if(input.left) control = 2;
-	else if(input.right) control = 3;
-	//else do nothing and keep control where it is
-
-	//shallow copy of input object
-	lastInput = {...input};
-
-	//console.log(control);
 }
 */
